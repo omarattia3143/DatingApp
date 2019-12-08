9d4ff16cb066fd1cf6d990f94a02bdc040598450
omar ashraf <omarattia3143@gmail.com>
2019-12-09 01:08:01 +0200

    Initial Commit

---
+++ DatingApp/Models/Value.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace DatingApp.Models
+{
+    public class Value
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+    }
+}
