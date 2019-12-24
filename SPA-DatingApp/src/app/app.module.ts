3fa228b4c3a6cdf67ef2810b4b6efd1c8e90084c
omar ashraf <omarattia3143@gmail.com>
2019-12-24 04:09:08 +0200

    end of section 4

--- SPA-DatingApp/src/app/app.module.ts
+++ SPA-DatingApp/src/app/app.module.ts
@@ -2,19 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { HttpClientModule} from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+import {FormsModule} from '@angular/forms';
+import {AuthService} from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import {RegisterModel} from './register/RegisterModel';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ValueComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule
+    ],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
